4dc85f53124fd1eec0e9e7ebcaf30148fa072e1d
Almásy Gergő <gergo.almasy@agroninja.com>
2019-05-17 17:30:55 +0200

    zxing

--- src/app/test/test.module.ts
+++ src/app/test/test.module.ts
@@ -3,10 +3,16 @@ import {CommonModule} from '@angular/common';
 import {HubCommonModule} from '../hub-common/hub-common.module';
 import {TestComponent} from './test/test.component';
 import {TestRoutingModule} from './test-routing.module';
+import {ZXingScannerModule} from '@zxing/ngx-scanner';
 
 @NgModule({
   declarations: [TestComponent],
-  imports: [CommonModule, HubCommonModule, TestRoutingModule],
+  imports: [
+    CommonModule,
+    HubCommonModule,
+    TestRoutingModule,
+    ZXingScannerModule
+  ],
   exports: [TestComponent]
 })
 export class TestModule {}
